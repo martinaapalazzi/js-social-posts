6e1d3bb39ba7e4bd9ad1b9b17093bffd2764602f
martinaapalazzi <martinaapalazzi@gmail.com>
2023-12-20 16:32:49 +0100

    Milestone 1

---
+++ script.js
@@ -0,0 +1,105 @@
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Sofia Perlari",
+            "image": "https://unsplash.it/300/300?image=10"
+        },
+        "likes": 120,
+        "created": "2021-09-03"
+    },
+    {
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Chiara Passaro",
+            "image": "https://unsplash.it/300/300?image=20"
+        },
+        "likes": 78,
+        "created": "2021-05-15"
+    },
+    {
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=24",
+        "author": {
+            "name": "Luca Formicola",
+            "image": null
+        },
+        "likes": 56,
+        "created": "2021-04-03"
+    },
+    {
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=534",
+        "author": {
+            "name": "Alessandro Sainato",
+            "image": "https://unsplash.it/300/300?image=29"
+        },
+        "likes": 95,
+        "created": "2021-03-05"
+    }
+];
+
+let postsContainer = document.querySelector(".post-container")
+
+for (let i = 0; i < posts.length; i++) {
+
+    postsContainer.innerHTML += `
+    <div class="posts">
+        <div class=header-flex>
+            <div class= "profile-pic-header">
+                <img src= "${posts[i]['author']['image']}" alt="">
+            </div>
+            <div class="description-column-flex">
+                <h3>
+                    ${posts[i]['author']['name']}
+                </h3>
+                <h5>
+                    ${posts[i]['created']}
+                </h5>
+            </div>
+        </div>
+        <div class= "content-container">
+            <div>
+                <p>
+                    ${posts[i]['content']}
+                </p>
+            </div>
+            <div>
+                <img src= "${posts[i]['media']}" alt="">
+            </div>
+        </div>
+        <div class= "footer-contaier">
+            <div>
+                <a href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span>Mi Piace</span>
+                </a>
+            </div>
+            <div>
+                ${posts[i]['likes']}
+            </div>
+        </div>
+    </div>
+`;
+
+
+
+}
+
